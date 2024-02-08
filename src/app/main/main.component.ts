4c192f76175dac26f38f2edf6d2ae221ffaccb0f
unknown <sakshi gatkine>
2024-02-08 16:05:16 +0530

    changes

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-main',
   standalone: true,
@@ -9,4 +10,5 @@ import { Component } from '@angular/core';
 })
 export class MainComponent {
 
+  
 }
